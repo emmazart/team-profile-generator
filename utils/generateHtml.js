491691fb5d6440a8f3746c13c5de061c4def3053
Emma Olson <emma.olson794@gmail.com>
2022-05-12 16:54:15 -0500

    separate manager and employee questions

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,8 +1,29 @@
 const generateHtml = function(data) {
     return `
-    
-    <h1>${data.name}</h1>
-    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+
+        <main>
+            <h2>${data.name}</h2>
+            <h3>${data.typeofEmployee}</h3>
+            <p>${data.id}</p>
+            <p>${data.email}</p>
+            <p>${data.github}</p>
+        </main>
+
+    </body>
+    </html>
+        
     `;
 };
 
