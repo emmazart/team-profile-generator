334cde8b8a83b9e150097ca99e703626b9bf051b
Emma Olson <emma.olson794@gmail.com>
2022-05-12 17:49:11 -0500

    additional intern tests pass

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,9 +1,21 @@
 const Intern = require('../lib/Intern');
 
 test('creates new intern object', () => {
-    const intern = new Intern('Anna', 'emma.olson794@gmail.com', 'UofM');
+    const intern = new Intern('Anna', 'emma.olson794@gmail.com', 1, 'UofM');
 
     expect(intern.name).toBe('Anna');
     expect(intern.email).toBe('emma.olson794@gmail.com');
     expect(intern.school).toBe('UofM');
+});
+
+test('getSchool returns intern school name', () => {
+    const intern = new Intern('Anna', 'emma.olson794@gmail.com', 1, 'UofM');
+
+    expect(intern.getSchool()).toBe('UofM');
+});
+
+test('getRole returns intern', () => {
+    const intern = new Intern('Anna', 'emma.olson794@gmail.com', 1, 'UofM');
+
+    expect(intern.getRole()).toBe('Intern');
 });
