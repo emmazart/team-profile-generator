c5abee2737307a0aba2ee50d5ed2f1873194d14f
Emma Olson <emma.olson794@gmail.com>
2022-05-12 17:31:35 -0500

    update employee tests with additional methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,50 +2,53 @@ const Employee = require('../lib/Employee');
 
 // test creation of new employee with name and email
 test('creates new employee object', () => {
-    const employee = new Employee('Anna', 'emma.olson794@gmail.com');
+    const employee = new Employee('Anna', 'emma.olson794@gmail.com', 1);
 
     expect(employee.name).toBe('Anna');
     expect(employee.email).toBe('emma.olson794@gmail.com');
-
-    console.log(employee);
+    expect(employee.id).toBe(1)
 });
 
-// test generating of employee id based on array length
-test('generate new and unique employee id', () => {
-    const employee = new Employee('Anna', 'emma.olson794@gmail.com');
-    employee.generateEmployeeId();
-    let employeeArray = [];
+// // test pushing new employee object to the array
+// test('new employee object added to employeesArray', () => {
+//     const employee = new Employee('Anna', 'emma.olson794@gmail.com', 1);
+//     employee.addToArray(employee);
 
-    if (employeeArray.length === 0) {
-        expect(employee.id).toEqual(1);
-    }
+//     // expect(employeesArray).toContain
+// });
+
+test('getname function returns employee name', () => {
+    const employee = new Employee('Anna', 'emma.olson794@gmail.com', 1);
+
+    expect(employee.getName(employee)).toEqual('Anna');
 });
 
+test('getid function returns employee id', () => {
+    const employee = new Employee('Anna', 'emma.olson794@gmail.com', 1);
+
+    expect(employee.getId(employee)).toEqual(1);
+});
+
+test('getemail function returns employee email', () => {
+    const employee = new Employee('Anna', 'emma.olson794@gmail.com', 1);
+
+    expect(employee.getEmail(employee)).toEqual('emma.olson794@gmail.com');
+});
+
+test('getrole function returns "employee"', () => {
+    const employee = new Employee('Anna', 'emma.olson794@gmail.com', 1);
+
+    expect(employee.getRole(employee)).toEqual('Employee');
+});
+
+
 // // test generating of employee id based on array length
-// test('generate new and unique employee id if array = 1', () => {
+// test('generate new and unique employee id', () => {
 //     const employee = new Employee('Anna', 'emma.olson794@gmail.com');
 //     employee.generateEmployeeId();
-//     let employeeArray = ['firstemployee'];
+//     let employeeArray = [];
 
-//     if (employeeArray.length === 1) {
-//         expect(employee.id).toEqual(2);
+//     if (employeeArray.length === 0) {
+//         expect(employee.id).toEqual(1);
 //     }
 // });
-
-// // test generating of employee id based on array length
-// test('generate new and unique employee id if array = 2', () => {
-//     const employee = new Employee('Anna', 'emma.olson794@gmail.com');
-//     employee.generateEmployeeId();
-//     let employeeArray = ['firstemployee', 'secondemployee'];
-
-//     if (employeeArray.length === 2) {
-//         expect(employee.id).toEqual(3);
-//     }
-// });
-
-
-// test pushing new employee object to the array
-test('new employee object added to employeesArray', () => {
-    const employee = new Employee('Anna', 'emma.olson794@gmail.com');
-    employee.addToArray(employee);
-})
