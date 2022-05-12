894eb94f102bfa1d0b1ef46eb9c969736212eeac
Emma Olson <emma.olson794@gmail.com>
2022-05-12 17:45:46 -0500

    additional enginner tests pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,22 @@
 const Engineer = require('../lib/Engineer');
 
 test('creats new engineer object', () => {
-    const engineer = new Engineer('Anna', 'emma.olson794@gmail.com', 'emmazart');
+    const engineer = new Engineer('Anna', 'emma.olson794@gmail.com', 1, 'emmazart');
 
     expect(engineer.name).toBe('Anna');
     expect(engineer.email).toBe('emma.olson794@gmail.com');
+    expect(engineer.id).toBe(1);
     expect(engineer.github).toBe('emmazart');
+});
+
+test('getGithub() returns github username', () => {
+    const engineer = new Engineer('Anna', 'emma.olson794@gmail.com', 1, 'emmazart');
+
+    expect(engineer.getGithub()).toBe('emmazart');
+});
+
+test('getrole returns engineer', () => {
+    const engineer = new Engineer('Anna', 'emma.olson794@gmail.com', 1, 'emmazart');
+
+    expect(engineer.getRole()).toBe('Engineer');
 });
