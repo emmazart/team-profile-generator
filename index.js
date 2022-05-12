977dedad406b048c053b45765dbf49cbd1a8fd9b
Emma Olson <emma.olson794@gmail.com>
2022-05-12 18:05:46 -0500

    create new employees based on user input

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+
+
 // create a function to write an html file
 function writeToFile(data) {
     // write to index file in dist directory
     fs.writeFile('./dist/index.html', data, err => {
         if (err) throw err;
-        console.log("index.html file has been written!");
+        console.log("Check the /dist folder for your complete Team Profile!");
     });
 };
 
@@ -25,13 +27,23 @@ function init() {
     inquirer // on init, run inquirer prompt
         .prompt(managerQuestions)
         .then(function(answer) { // then handle the response
-            console.log(answer)
-            writeToFile(generateHtml(answer)); // send response to html file
+            const manager = new Manager(answer.name, answer.email, answer.id, answer.office);
+            console.log(manager);
 
             inquirer.prompt(questions)
             .then(function(answer) {
-                console.log(answer)
-                writeToFile(generateHtml(answer)); // send response to html file
+
+                if (answer.typeofEmployee === 'Engineer'){
+                    const engineer = new Engineer(answer.name, answer.email, answer.id, answer.github);
+                    console.log(engineer);
+                }
+
+                if (answer.typeofEmployee === 'Intern'){
+                    const intern = new Intern(answer.name, answer.email, answer.id, answer.school);
+                    console.log(intern);
+                }
+
+                // writeToFile(generateHtml(answer)); // send response to html file
             })
     
         })
